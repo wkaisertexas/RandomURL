1b7209043d19125de4fd2f0c5cf1da9f74a1daa5
William Kaiser <wkaisertexas@gmail.com>
2021-10-10 12:21:05 -0500

    Update settings.py

--- RandomURL/settings.py
+++ RandomURL/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
+DISABLE_COLLECTSTATIC = 1 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
