3e59207d762e7835a75339740cf0968e336c43aa
William Kaiser <wkaisertexas@gmail.com>
2022-03-23 17:34:29 -0500

    Update sitemaps.py

--- url/sitemaps.py
+++ url/sitemaps.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
-    changefreq = 'monthly'
+    changefreq = 'weekly'
 
     def items(self):
         return ['index', 'login', 'account-creation', 'privacy', 'tos']
